6890ced7cff8ae4d68238d69a52497220de346c1
minah741 <minah741@naver.com>
2024-08-01 23:54:25 +0900

    [chore] init project
    - Spring Boot 초기 프로젝트 업로드
    - gitignore 설정

---
+++ src/main/java/com/freeboard/petchive/PetchiveApplication.java
@@ -0,0 +1,13 @@
+package com.freeboard.petchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freeboard/petchive/PetchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeboard.petchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
